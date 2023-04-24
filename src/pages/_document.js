97e32248ff360e6f3f6d23bd48dd34c2a68ee2b3
kalyanogirala1 <113006539+kalyanogirala1@users.noreply.github.com>
2023-04-25 02:48:03 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-mainBg w-full'>
+      <body className='bg-mainBg w-full md:overflow-hidden overflow-scroll'>
         <Main />
         <NextScript />
       </body>
